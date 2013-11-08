abfa1c69fb73644f6730e3eec14f7e7ad58aa30e
John 'Warthog9' Hawley <warthog9@eaglescrag.net>
2013-11-08 12:56:08 -0700

    Client: add basic v1 support (and version negotiation)

--- SyncDiff/Client.pm
+++ SyncDiff/Client.pm
@@ -13,6 +13,7 @@ extends qw(SyncDiff::Forkable);
 
 use SyncDiff::File;
 use SyncDiff::Util;
+use SyncDiff::Protocol::v1;
 
 #
 # Other Includes
@@ -22,6 +23,8 @@ use JSON::XS;
 use MIME::Base64;
 use IO::Socket;
 use IO::Handle;
+
+use Scalar::Util qw(looks_like_number);
 
 #
 # Debugging
@@ -67,10 +70,15 @@ has 'groupbase_path' => (
 
 has 'socket' => (
 		is	=> 'rw',
+		isa	=> 'IO::Socket::INET',
+		);
+
+has 'protocol_version' => (
+		is	=> 'rw',
 		isa	=> 'Str',
 		);
 
-has 'auth_token' => (
+has 'protocol_object' => (
 		is	=> 'rw',
 		isa	=> 'Str',
 		);
@@ -146,10 +154,109 @@ sub fork_and_connect {
 			print "Could not create socket: $!\n";
 			next;
 		} # end skipping if the socket is broken
+
+		$self->socket( $sock );
+
 		print $sock "Hello World!\n";
+		$self->request_protocol_versions();
 		close( $sock );
 	} # end foreach $host
 } # end fork_and_connect()
+
+sub request_protocol_versions {
+	my( $self ) = @_;
+
+	my %request = (
+		'operation'	=> 'request_protocol_versions',
+	);
+
+	my $versions = $self->basic_send_request( %request );
+
+	print Dumper $versions;
+
+	my $highest_proto_supported = "1.99";
+	my $proto_to_use = 0;
+
+	foreach my $ver ( @{$versions} ){
+		print "Version: ". $ver ."\n";
+
+		if( ! looks_like_number($ver) ){
+			print "*** $ver is not a version number\n";
+			next;
+		}
+
+		if(
+			$ver <= $highest_proto_supported
+			&&
+			$ver >= $proto_to_use
+		){
+			$proto_to_use = $ver;
+			print "Currently selected Protocol Version: ". $ver ."\n";
+		}
+	} # end foreach $ver
+
+	$self->protocol_version( $proto_to_use );	
+
+	my $protocol_obj;
+
+	if(
+		$proto_to_use >= 1.0
+		&&
+		$proto_to_use < 2.0
+	){
+		$protocol_obj = SyncDiff::Protocol::v1->new( socket => $self->socket, version => $proto_to_use );
+	}
+
+	$protocol_obj->setup();
+} # end request_protocol_version()
+
+sub basic_send_request {
+	my( $self, %request ) = @_;
+
+	my $json = encode_json( \%request );
+
+	my $socket = $self->socket;
+
+	print $socket $json ."\n";
+
+	my $line = undef;
+
+	while( $line = <$socket> ){
+		if( defined $line  ){
+			chomp( $line );
+			last if( $line ne "" );
+		}
+	}
+
+	chomp( $line );
+
+	if( $line eq "0" ){
+		return 0;
+	}
+
+	my $response = decode_json( $line );
+
+	print Dumper $response;
+	print "Ref: ". ref( $response ). "\n";
+
+	if( ref( $response ) eq "ARRAY" ){
+		return $response;
+	}
+
+	if( defined $response->{ZERO} ){
+		return 0;
+	}
+
+	if( defined $response->{SCALAR} ){
+		return $response->{SCALAR};
+	}
+
+	if( defined $response->{ARRAY} ){
+		return $response->{ARRAY};
+	}
+
+	return $response;
+} # end send_request()
 
 #no moose;
 __PACKAGE__->meta->make_immutable;
