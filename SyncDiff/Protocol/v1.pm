ab5d69d010b298df5a3a405ef81b1e682f1e5563
John 'Warthog9' Hawley <warthog9@eaglescrag.net>
2014-01-06 15:16:35 -0700

    Protocol v1: convert to using SenderReciever
    Same as last commit, basically a pile of code got deleted

--- SyncDiff/Protocol/v1.pm
+++ SyncDiff/Protocol/v1.pm
@@ -4,6 +4,8 @@ package SyncDiff::Protocol::v1;
 $SyncDiff::Protocol::v1::VERSION = '0.01';
 use Moose;
 
+extends qw(SyncDiff::SenderReciever);
+
 #
 # Other Includes
 #
@@ -55,54 +57,6 @@ sub setup {
 		exit(1);
 	}
 } # end setup()
-
-sub send_request {
-	my( $self, %request ) = @_;
-
-	my $json = encode_json( \%request );
-
-	my $socket = $self->socket;
-
-	print $socket $json ."\n";
-
-	my $line = undef;
-
-	while( $line = <$socket> ){
-		if( defined $line  ){
-			chomp( $line );
-			last if( $line ne "" );
-		}
-	}
-
-	chomp( $line );
-
-	if( $line eq "0" ){
-		return 0;
-	}
-
-	my $response = decode_json( $line );
-
-	print Dumper $response;
-	print "Ref: ". ref( $response ). "\n";
-
-	if( ref( $response ) eq "ARRAY" ){
-		return $response;
-	}
-
-	if( defined $response->{ZERO} ){
-		return 0;
-	}
-
-	if( defined $response->{SCALAR} ){
-		return $response->{SCALAR};
-	}
-
-	if( defined $response->{ARRAY} ){
-		return $response->{ARRAY};
-	}
-
-	return $response;
-} # end send_request()
 
 #
 # This is the main routine, once the protocol version
