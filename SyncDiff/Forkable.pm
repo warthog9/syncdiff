ab0a9b78f0a8e660e38e725db722c6b3f5c1b4e2
John 'Warthog9' Hawley <warthog9@eaglescrag.net>
2014-01-27 11:58:22 -0700

    Adding Licence information.  SyncDiff(erent) is now LGPL v2.1+

--- SyncDiff/Forkable.pm
+++ SyncDiff/Forkable.pm
@@ -1,5 +1,33 @@
 #!/usr/bin/perl
 
+###########################################################################
+# Copyright (C) 2014  John 'Warthog9' Hawley                              #
+#                         jhawley@redhat.com                              #
+#                         warthog9@eaglescrag.net                         #
+#                                                                         #
+# This file is originally part of SyncDiff(erent).                        #
+#                                                                         #
+# This library is free software; you can redistribute it and/or           #
+# modify it under the terms of the GNU Lesser General Public              #
+# License as published by the Free Software Foundation; either            #
+# version 2.1 of the License, or (at your option) any later version.      #
+#                                                                         #
+# This library is distributed in the hope that it will be useful,         #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       #
+# Lesser General Public License for more details.                         #
+#                                                                         #
+# You should have received a copy of the GNU Lesser General Public        #
+# License along with this library; if not, write to the:                  #
+#    Free Software Foundation, Inc.                                       #
+#    51 Franklin Street                                                   #
+#    Fifth Floor                                                          #
+#    Boston, MA  02110-1301                                               #
+#    USA                                                                  #
+#                                                                         #
+# Or, see <http://www.gnu.org/licenses/>.                                 #
+###########################################################################
+
 package SyncDiff::Forkable 0.01;
 use Moose;
 
