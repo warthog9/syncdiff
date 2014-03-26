c7406183f360df5519803aec148e5133c0b563d7
Greg Lund-Chaix <gchaix@squishymedia.com>
2014-03-26 16:08:04 -0700

    Fix Perl dependencies

--- vagrant/manifests/default.pp
+++ vagrant/manifests/default.pp
@@ -16,15 +16,16 @@ package { 'puppet':
 }
 
 # Ensure syncdiff prerequsites are present
-package { 'librsync-devel':
-  ensure  => 'present',
-}
-#package { 'librsync':
-#  ensure  => 'present',
-#}
-package { 'perl-CPAN':
-  ensure  => 'present',
-}
-package { 'perl-Module-Build':
-  ensure  => 'present',
-}
+package { 'librsync-devel': ensure  => 'present', }
+#package { 'librsync': ensure  => 'present', }
+package { 'perl-CPAN': ensure  => 'present', }
+
+package { 'perl-Module-Build': ensure => 'present', }
+package { 'perl-Moose': ensure => 'present', }
+package { 'perl-ParseLex': ensure => 'present', }
+package { 'perl-DBD-SQLite': ensure => 'present', }
+package { 'perl-Digest-SHA1': ensure => 'present', }
+package { 'perl-Parse-Yapp': ensure => 'present', }
+package { 'perl-File-FnMatch': ensure => 'present', }
+package { 'perl-Try-Tiny': ensure => 'present', }
+package { 'perl-JSON-XS': ensure => 'present', }
