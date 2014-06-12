98cb9db6e7a51218ef30fac2d036d39947e75986
Greg Lund-Chaix <gchaix@squishymedia.com>
2014-06-12 11:02:24 -0700

    Fix Vagrant provision errors on perl packages
    Brute-force install packages with yum instead of Puppet to avoid yum
    repo dependency issue.

--- vagrant/manifests/default.pp
+++ vagrant/manifests/default.pp
@@ -15,17 +15,3 @@ package { 'puppet':
   #require => Package['puppetlabs-release'],
 }
 
-# Ensure syncdiff prerequsites are present
-package { 'librsync-devel': ensure  => 'present', }
-#package { 'librsync': ensure  => 'present', }
-package { 'perl-CPAN': ensure  => 'present', }
-
-package { 'perl-Module-Build': ensure => 'present', }
-package { 'perl-Moose': ensure => 'present', }
-package { 'perl-ParseLex': ensure => 'present', }
-package { 'perl-DBD-SQLite': ensure => 'present', }
-package { 'perl-Digest-SHA1': ensure => 'present', }
-package { 'perl-Parse-Yapp': ensure => 'present', }
-package { 'perl-File-FnMatch': ensure => 'present', }
-package { 'perl-Try-Tiny': ensure => 'present', }
-package { 'perl-JSON-XS': ensure => 'present', }
