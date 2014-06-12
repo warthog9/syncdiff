059c558f9af9dc943aeb18c9c80978e1425564f1
Greg Lund-Chaix <gchaix@squishymedia.com>
2014-06-12 11:06:17 -0700

    Remove unneeded/confusing comment

--- vagrant/packages.sh
+++ vagrant/packages.sh
@@ -7,19 +7,3 @@ yum install -y -q librsync-devel librsync perl-CPAN perl-Module-Build perl-Moose
 if [ ! -f /usr/local/bin/cpanm ]; then
   curl -L http://cpanmin.us | perl - --sudo App::cpanminus
 fi
-
-
-  # Ensure syncdiff prerequsites are present
-  #   package { 'librsync-devel': ensure  => 'present', }
-  #     #package { 'librsync': ensure  => 'present', }
-  #       package { 'perl-CPAN': ensure  => 'present', }
-  #
-  #         package { 'perl-Module-Build': ensure => 'present', }
-  #           package { 'perl-Moose': ensure => 'present', }
-  #           ~ #package { 'perl-ParseLex': ensure => 'present', }
-  #             package { 'perl-DBD-SQLite': ensure => 'present', }
-  #               package { 'perl-Digest-SHA1': ensure => 'present', }
-  #                 package { 'perl-Parse-Yapp': ensure => 'present', }
-  #                 ~ #package { 'perl-File-FnMatch': ensure => 'present', }
-  #                   package { 'perl-Try-Tiny': ensure => 'present', }
-  #                     package { 'perl-JSON-XS': ensure => 'present', }
